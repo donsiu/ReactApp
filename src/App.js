64f17b1b8fbbaf24494a4687e37544c8e6551091
donsiu <spcdonsiu@gmail.com>
2017-12-26 15:54:01 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -8,26 +8,60 @@ class App extends Component {
     persons : [{ name : `BB0`, age : 20},
                { name : `BB1`, age : 21},
                { name : `BB2`, age : 22}],
-    otherState : null
+    otherState : null,
+    showPersons : false,
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({ 
+      persons : [{ name : event.target.value, age : 20},
+               { name : `Manu`, age : 21},
+               { name : `Max`, age : 22}]   
+    })
+  } 
+
+  togglePersonHanlder = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons : !doesShow});
   }
 
   switchName = (newName) => {
     this.setState({    
-    persons : [{ name : `Donald`, age : 20},
+    persons : 
+    [{ name : `Donald`, age : 20},
     { name : `Siu`, age : 21},
     { name : newName, age : 233}]})
   }
 
   render() {
-    return (
+    //inline style
+    const style = {
+      backgroundColor: 'White',
+      fonrt:'inherit',
+      border: '1x solid blue',
+      padding: '8px',
+      cusor: 'pointer'
+    }
+
+    let persons = null;
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+             return (<Person 
+             name= {person.name} 
+             age= {person.age}/>)
+          })}
+          {/* Synetic event  https://reactjs.org/docs/events.html#clipboard-events */}
+        </div>);
+    }
+
+    return(
       <div className="App">
         <h1>Hello</h1>
-        <h1>Another heading</h1>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          {/* Synetic event  https://reactjs.org/docs/events.html#clipboard-events */}
-        <button onClick={this.switchName.bind(this, "hello boboie")}>Switch Name</button>
+        {/* this.switchName.bind(this, "hello bobby") */}
+        <button style={style} onClick={this.togglePersonHanlder}>Toggle Name</button>
+        {persons}
       </div>
     );
     return React.createElement('div', {className : 'App'}, React.createElement('h1',null, 'hi, I am new'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './Person.css';
+
 // function component
 const person = (data) => {
-return (
-    <div>
-        <p>hello I'm {data.name}, and am {data.age}</p>
-        <p>{data.children}</p>
-    </div>
-);
+    return (
+        <div className="Person">
+            <p>hello I'm {data.name}, and am {data.age}</p>
+            <p>{data.children}</p>
+            {/* onChange the attribut of HTML */}
+            <input type="text" onChange={ data.changehaha} value={ data.name}/> 
+        </div>
+    );
 }
 
 // function person(){
